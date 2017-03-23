9960bd7d990488b44d02c7abcff45d53506028bc
Silvia Amabilino <walfits@gmail.com>
2017-03-23 18:36:19 +0000

    fitting of Simon CASSCF data

--- scratch/example_NN02.py
+++ scratch/example_NN02.py
@@ -10,51 +10,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ### --------------- ** Importing the data ** -----------------
-importData.XYZtoCSV("/Users/walfits/Repositories/trainingdata/per-user-trajectories/CH4+CN/combinedTraj.xyz")
+importData.XMLtoCSV("/Users/walfits/Repositories/tensorflow/AMP/input1.xml")
 
 X_total = importData.loadX("X.csv")
 Y_total = importData.loadY("Y.csv")
 
+angles = np.arange(175, 97.5, -2.5)
+angles = angles.reshape(Y_total.shape)
+Y_total = np.concatenate((Y_total, angles), axis=1)
+
+
+### --------------- ** Interpolating ** -----------------------
+
+# X_total, Y_total = importData.interpolData(X_total, Y_total, 10)
 
 ### --------------- ** Turning cartesian coordinates into coulomb matrix ** -----------------
 
 descriptor = CoulombMatrix.CoulombMatrix(matrixX=X_total)
 descriptor.generate()
+descriptor.normalise_2()
 
 ### --------------- ** Splitting the data into training, cross-validation and validation set ** -----------------
 
-dataProportions = np.array([0.9, 0.1, 0])
+dataProportions = np.array([1, 0, 0])
 splitX, splitY = importData.splitData(descriptor.coulMatrix, Y_total, dataProportions)
 
 X_trainSet = splitX[0]
 Y_trainSet = splitY[0]
+reshapeY = np.reshape(Y_trainSet[:,0], (Y_trainSet[:,0].shape[0], 1))
 
 X_crossVal = splitX[1]
 Y_crossVal = splitY[1]
+reshapeYcross = np.reshape(Y_crossVal[:,0], (Y_crossVal[:,0].shape[0], 1))
 
 X_val = splitX[2]
 Y_val = splitY[2]
 
 ### --------------- ** Training the neural network ** -----------------
 
-method = NN_02.NeuralNetwork(n_hidden_layer=20, learning_rate=0.05, iterations=80, eps=0.01)
+method = NN_02.NeuralNetwork(n_hidden_layer=50, learning_rate=0.0005, iterations=50000, eps=0.001)
 
-betaGrid = np.arange(0.005,0.027, 0.002)
-trainingCost = []
-testCost = []
+method.fit(X_trainSet, reshapeY, beta=0, batch_size=10, plot=True)
 
-currentTrainCost = method.fit(X_trainSet, Y_trainSet, beta=0.02, batch_size=10, plot=True)
-trainingCost.append(currentTrainCost)
-currentTestCost = method.costTest(X_crossVal, Y_crossVal)
-testCost.append(currentTestCost)
+predictions = method.predict(X_trainSet)
 
-print trainingCost
-print testCost
+fig1 = plt.figure(figsize=(7,7))
+ax1 = fig1.add_subplot(111)
 
-# plt.plot(betaGrid, trainingCost, label = "training cost", c="red")
-# plt.plot(betaGrid, testCost, label = "test cost", c="blue")
-# plt.legend(loc="upper right")
-# plt.show()
+x1 = Y_trainSet[:,1]
+y_data = Y_trainSet[:,0]
+y_pred = predictions[:,0]
+
+ax1.scatter(x1, y_data, label = "actual data", marker=".", c="r")
+ax1.scatter(x1, y_pred, label = "predictions", marker=".", c='b')
+
+ax1.set_xlabel('CHC angle')
+ax1.set_ylabel('Energy')
+
+
+plt.show()
 
 
 
