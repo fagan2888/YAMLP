4d5fb18b92e80ab7a2ef7a0a0e1f2de26a0cdb91
Silvia Amabilino <walfits@gmail.com>
2017-04-03 21:34:14 +0100

    Made it in such a way that the parameters can be stored and training can be continued.

--- scratch/NN_02.py
+++ scratch/NN_02.py
@@ -19,6 +19,7 @@ class NeuralNetwork():
         self.learning_rate = learning_rate
         self.iterations = iterations
         self.eps = eps
+        self.initialisation = True
 
         self.X_trainSet = np.array(1)
         self.Y_trainSet = np.array(1)
@@ -33,6 +34,10 @@ class NeuralNetwork():
         self.w2 = 0
         self.b2 = 0
 
+        # This will contain the cost as the training set is fitted
+        self.trainCost = []
+        self.CrossValCost = []
+
     def modelNN(self, X_train, parameters):
         """
         This function calculates the model neural network
@@ -43,7 +48,7 @@ class NeuralNetwork():
 
         # Definition of the model
         a1 = tf.matmul(X_train, tf.transpose(parameters[0])) + parameters[1]  # output of layer1, size = n_sample x n_hidden_layer
-        a1 = tf.nn.tanh(a1)
+        a1 = tf.nn.sigmoid(a1)
         model = tf.matmul(a1, tf.transpose(parameters[2])) + parameters[3]  # output of last layer, size = n_samples x 1
 
         return model
@@ -67,10 +72,10 @@ class NeuralNetwork():
         """
         This function calculates the weights and biases that better fit the data
         :param X: This is a numpy 2D array of size (n_samples, n_features)
-        :param Y: This is a numby 2D array of size (n_samples, 1)
+        :param Y: This is a numpy 2D array of size (n_samples, 1)
         :beta: regularisation parameter
         :batch_size: size of the batch for mini-batch gradient descent
-        :plot: a boolean that tells whether to plot or not
+        :plot: a boolean that tells whether to plot the cost or not
         :return: None
         """
         print "Starting the fitting. \n"
@@ -84,27 +89,28 @@ class NeuralNetwork():
 
         randIn = tf.constant(self.eps, dtype=tf.float32)  # This sets the range of weights/biases initial values
 
-        weights1 = tf.Variable(tf.random_normal([self.n_hidden_layer, self.n_feat]) * 2 * randIn - randIn)
-        bias1 = tf.Variable(tf.zeros([self.n_hidden_layer]))
-        weights2 = tf.Variable(tf.random_normal([1, self.n_hidden_layer]) * 2 * randIn - randIn)
-        bias2 = tf.Variable(tf.zeros([1]))
+        if self.initialisation:
+            weights1 = tf.Variable(tf.random_normal([self.n_hidden_layer, self.n_feat]) * 2 * randIn - randIn)
+            bias1 = tf.Variable(tf.zeros([self.n_hidden_layer]))
+            weights2 = tf.Variable(tf.random_normal([1, self.n_hidden_layer]) * 2 * randIn - randIn)
+            bias2 = tf.Variable(tf.zeros([1]))
+            parameters = [weights1, bias1, weights2, bias2]
+            self.initialisation = False
+        else:
+            parameters = [tf.Variable(self.w1), tf.Variable(self.b1), tf.Variable(self.w2), tf.Variable(self.b2)]
 
-        model = self.modelNN(X_train, [weights1, bias1, weights2, bias2])
-
-        cost = self.costReg(model, Y_train, [weights1, weights2], beta)
+        model = self.modelNN(X_train, parameters)
+        cost = self.costReg(model, Y_train, [parameters[0], parameters[2]], beta)
 
         optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(cost)
 
         # Initialisation of the model
         init = tf.initialize_all_variables()
-        cost_array = []
+
 
         # Running the graph
         with tf.Session() as sess:
             sess.run(init)
-            # for iter in range(self.iterations):
-            #     opt, c = sess.run([optimizer, cost], feed_dict={X_train: X, Y_train: Y})
-            #     cost_array.append(c)
 
             for iter in range(self.iterations):
                 # This is the total number of batches in which the training set is divided
@@ -116,24 +122,29 @@ class NeuralNetwork():
                     batch_y = Y[i * batch_size:(i + 1) * batch_size, :]
                     opt, c = sess.run([optimizer, cost], feed_dict={X_train: batch_x, Y_train: batch_y})
                     avg_cost += c / n_batches
-                cost_array.append(avg_cost)
+                self.trainCost.append(avg_cost)
 
-            self.w1 = sess.run(weights1)
-            self.b1 = sess.run(bias1)
-            self.w2 = sess.run(weights2)
-            self.b2 = sess.run(bias2)
+            self.w1 = sess.run(parameters[0])
+            self.b1 = sess.run(parameters[1])
+            self.w2 = sess.run(parameters[2])
+            self.b2 = sess.run(parameters[3])
 
             print "The value of the cost with beta=" + str(beta) + " is "
-            print str(cost_array[-1])
+            print str(self.trainCost[-1])
 
         if plot:
             import matplotlib.pyplot as plt
-            plt.plot(cost_array)
+            plt.plot(self.trainCost)
             plt.show()
 
         return None
 
     def predict(self, X):
+        """
+        This function uses the X data and plugs it into the model and then returns the predicted Y
+        :param X: numpy array of size (n_samples, n_features)
+        :return: numpy 2D array of size (n_samples, 1)
+        """
 
         X_test = tf.placeholder(tf.float32, [None, self.n_feat])
 
@@ -149,6 +160,13 @@ class NeuralNetwork():
         return results
 
     def costTest(self, X, Y):
+        """
+        This function calculates the cost on a dataset X and returns it. It also appends it to an array so that if done
+        during the fitting it can be used to plot learning curves.
+        :param X: numpy array of size (n_samples, n_features)
+        :param Y: numpy array of size (n_samples, 1)
+        :return:
+        """
         X_test = tf.placeholder(tf.float32, [None, self.n_feat])
         Y_test = tf.placeholder(tf.float32, [None, 1])
 
@@ -161,6 +179,8 @@ class NeuralNetwork():
         with tf.Session() as sess:
             sess.run(init)
             testCost = sess.run(cost, feed_dict={X_test: X, Y_test: Y})
+
+        self.CrossValCost.append(testCost)
 
         return testCost
 
