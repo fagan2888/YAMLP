5f82041946e40e5380529af0d0a87c0999aa2b74
Silvia Amabilino <walfits@gmail.com>
2017-06-06 16:30:27 +0100

    A script that reproduces what was done in the Hackathon but with tensorflow

---
+++ SciFlow/hackathonExample.py
@@ -0,0 +1,40 @@
+import CoulombMatrix
+import ImportData
+import NNFlow
+import plotting
+import numpy as np
+from sklearn import preprocessing as preproc
+from sklearn import model_selection as modsel
+
+
+
+# Importing the data
+X, y = ImportData.loadPd("hackathonData.csv")
+
+# Creating the CM object
+coulMat = CoulombMatrix.CoulombMatrix(matrixX=X)
+#  Creating the three descriptors
+# eigSpec = coulMat.generateES()
+# sortMat = coulMat.generateSCM()
+ranSorMat, y = coulMat.generateRSCM(y_data=y, numRep=4)
+
+# Normalising the data
+X_scal = preproc.StandardScaler().fit_transform(ranSorMat)
+
+# Split into training and test set
+X_train, X_test, y_train, y_test = modsel.train_test_split(X_scal, y, test_size=0.1)
+
+
+# Defining the estimator
+estimator = NNFlow.MLPRegFlow(hidden_layer_sizes=(46,),alpha=0.3,learning_rate_init=0.0001,max_iter=500)
+
+# Fitting the model
+estimator.fit(X_train,y_train)
+estimator.plotTrainCost()
+
+# Calculating the predictions
+y_pred = estimator.predict(X_test)
+
+# Correlation plot
+plotting.plotSeaborn(y_test, y_pred)
+
