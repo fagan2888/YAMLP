d9f0ddd0c1fb2e6a5074e1d3417b165e83a87289
Silvia Amabilino <walfits@gmail.com>
2017-06-22 14:52:18 +0100

    Added option to change the limits of the plot.

--- SciFlow/plotting.py
+++ SciFlow/plotting.py
@@ -1,7 +1,7 @@
 import seaborn as sns
 import pandas as pd
 
-def plotSeaborn(list1, list2):
+def plotSeaborn(list1, list2, ylim=(1.90, 1.78), xlim=(1.90, 1.78)):
     """
     :param list1: List of Original values
     :param list2: List of NN predicted values
@@ -12,7 +12,7 @@ def plotSeaborn(list1, list2):
     df['High level calculated energies (Ha)'] = list1
     df['NN predicted energies (Ha)'] = list2
     lm = sns.lmplot('High level calculated energies (Ha)','NN predicted energies (Ha)', data=df, scatter_kws={"s": 20, "alpha": 0.6}, line_kws={"alpha":0.5})
-    lm.set(ylim=(1.90, 1.78))
-    lm.set(xlim=(1.90, 1.78))
+    lm.set(ylim=ylim)
+    lm.set(xlim=xlim)
 
     sns.plt.show()
