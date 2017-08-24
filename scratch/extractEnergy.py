30e206fabb91605fad22bfd4d7c7f80ad8740128
Silvia Amabilino <walfits@gmail.com>
2017-08-24 09:45:30 +0100

    Cleaning up before merging

--- scratch/extractEnergy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def getEnergy(fileY):
-    energiesList = []
-    for line in fileY:
-        energiesList.append(float(line))
-
-    energies = np.asarray(energiesList).reshape((len(energiesList), 1))
-    return energies
